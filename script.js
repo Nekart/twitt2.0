e6a0a8811c54ce46aad7f9ee75877e6f9f8fda90
Gt86Factory <68557524+Gt86Factory@users.noreply.github.com>
2020-12-01 14:31:12 -0400

    new

---
+++ script.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+$("td").on({click: function(){
+    $(this).css("color", "#1da1f2",);}
+                                     });
+                                        });
